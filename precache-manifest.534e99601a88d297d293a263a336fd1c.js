ef39fd1a9edebd3ed482e4669aa3323930559228
Margo Astreyko <margoastreyko@mail.ru>
2020-10-03 17:20:20 +0300

    Updates

---
+++ precache-manifest.534e99601a88d297d293a263a336fd1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69827779e1877e3b50ee3791dfb01cb",
+    "url": "/g-analytics/index.html"
+  },
+  {
+    "revision": "778409cbe9eeab9a8e7f",
+    "url": "/g-analytics/static/css/main.0a562a19.chunk.css"
+  },
+  {
+    "revision": "b605be6bdc2a8e5e5621",
+    "url": "/g-analytics/static/js/2.dddcd0dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/g-analytics/static/js/2.dddcd0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "778409cbe9eeab9a8e7f",
+    "url": "/g-analytics/static/js/main.eab48b4e.chunk.js"
+  },
+  {
+    "revision": "5baa374cc8a1c596aa43",
+    "url": "/g-analytics/static/js/runtime-main.7c8de267.js"
+  }
+]);
